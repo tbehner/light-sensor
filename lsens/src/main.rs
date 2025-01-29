11fc7cdd629d57e70ae1b23c0e1873a4a7fec55a
Timm Behner <timm.behner@gmail.com>
2025-01-29 07:01:45 +0100

    better variable name

--- lsens/src/main.rs
+++ lsens/src/main.rs
@@ -18,14 +18,14 @@ fn main() -> ! {
 
     loop {
         arduino_hal::delay_ms(1000);
-        let voltage = sensor_input.analog_read(&mut adc);
+        let light_value = sensor_input.analog_read(&mut adc);
 
-        if voltage < 400 && led.is_set_low(){
+        if light_value < 400 && led.is_set_low(){
             led.set_high();
-        } else if voltage > 500 && led.is_set_high() {
+        } else if light_value > 500 && led.is_set_high() {
             led.set_low();
         }
 
-        let _ = ufmt::uwriteln!(serial, "{}", voltage);
+        let _ = ufmt::uwriteln!(serial, "{}", light_value);
     }
 }
